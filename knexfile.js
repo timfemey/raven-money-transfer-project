bed8a6275adf72c3b8247c4dda2b0a0d15168ffa
timfemey <isholaobafemi@gmail.com>
2025-07-11 19:57:49 +0100

    removed directory for  knex migrations function

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
 import "dotenv/config"
 
-export default { client: "mysql2", connection: { host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME }, migrations: { database: "./database/migrations" } }
+export default { client: "mysql2", connection: { host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME } }
 
