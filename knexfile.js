058cfab437ced671bc67c581bbeca36b665e13e1
timfemey <isholaobafemi@gmail.com>
2025-07-14 16:36:30 +0100

    add PORT

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
 import "dotenv/config"
 
-export default { client: "mysql2", connection: { host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME } }
+export default { client: "mysql2", connection: { port:process.env.DB_PORT, host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME } }
 
